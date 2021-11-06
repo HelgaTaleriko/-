dd5f7e2bb6444dcddbc651dda03974d14185442d
Helga <89998178494@mail.ru>
2021-11-06 23:56:44 +0300

    Добавьте файлы проекта.

---
+++ проверка/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace проверка
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Calculator calc1 = new Calculator();
+                Console.WriteLine("введите неотрицательное число");
+                int input = Convert.ToInt32(Console.ReadLine());
+                int resultFact = calc1.Factorial(input);
+                int resultFibo = calc1.Fibo(input);
+                int resultFactRec = calc1.FactorialRecursive(input);
+                int[] result = new int[] { resultFact, resultFibo, resultFactRec };
+                Console.WriteLine("Результат Фаакториала: " + result[0]);
+                Console.WriteLine("Результат Фибоначи: " + result[1]);
+                Console.WriteLine("Результат рекурсивного Фаакториала: " + result[2]);
+            }
+        }
+    }
+
+}
